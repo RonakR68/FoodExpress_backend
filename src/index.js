103ccfd708bb516ca958c6e8d2512c7affe9f5fd
Ronak <redkarronak@gmail.com>
2024-07-16 23:17:38 +0530

    added user register, login, logout using jwt http only cookie

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+//import myUserRoute from "./routes/MyUserRoute";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+const port = process.env.PORT || 7000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to mongodb"));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/status", async (req, res) => {
+  res.send({ message: "status OK!" });
+});
+
+app.use('/api/auth', authRoutes);
+//forward request to myUserRoute
+//app.use("/api/my/user", myUserRoute);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`server started on localhost: ${port}`);
+});
