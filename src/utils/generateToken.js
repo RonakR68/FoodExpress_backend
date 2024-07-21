b8bc65b6e9b4da27b7b4eb51fc683c0aaf755ec7
Ronak <redkarronak@gmail.com>
2024-07-22 00:16:53 +0530

    cookie domain

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -12,7 +12,7 @@ const generateToken = (res, userId) => {
     secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
     sameSite: 'None', // Prevent CSRF attacks
     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
-    domain: process.env.COOKIE_DOMAIN,
+    //domain: process.env.COOKIE_DOMAIN,
   });
 };
 
