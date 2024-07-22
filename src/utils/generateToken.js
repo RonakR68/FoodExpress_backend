b5c040f66391a6884e9d09af04fb051b584c7f08
Ronak <redkarronak@gmail.com>
2024-07-22 18:41:13 +0530

    env file changes

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,17 +1,19 @@
 import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
 
-const expiry_duration = process.env.TOKEN_EXPIRY_DURATION;
+const expiry_duration = process.env.COOKIE_EXPIRY_DURATION;
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
+    expiresIn: `${expiry_duration}d`,
   });
 
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    secure: process.env.ENV === 'production', // Use secure cookies in production
     sameSite: 'None', // Prevent CSRF attacks
-    maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+    maxAge: expiry_duration * 24 * 60 * 60 * 1000, // 1 day in milliseconds
     //domain: process.env.COOKIE_DOMAIN,
   });
 };
