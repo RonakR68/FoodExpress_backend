bcd4196659eeba4177a33debf87f649981c3c370
Ronak <redkarronak@gmail.com>
2024-07-23 16:44:03 +0530

    cors changes for deployment

--- src/index.js
+++ src/index.js
@@ -36,9 +36,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.options('*', cors(corsOptions));
+//app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Preflight CORS handler
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', client_base_url);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.status(200).json({ body: "OK" });
+});
 
 
 
